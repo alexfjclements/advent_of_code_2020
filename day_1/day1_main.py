409985254659921d5b40b025a53f6b1cbacd8fbe
Alexander Clements <alexfjclements@gmail.com>
2020-12-05 11:07:35 +0000

    day 1 part 2 completed

--- day_1/day1_main.py
+++ day_1/day1_main.py
@@ -2,10 +2,24 @@ from day_1.day1_input import day1_input_data
 
 result = []
 
+# Part 1
+# for x in day1_input_data:
+#     for y in day1_input_data:
+#         if x != y and x + y == 2020:
+#             result.append(x*y)
+#
+# if result[0] == result[1]:
+#     print(result[0])
+# else:
+#     print('Error')
+
+# Part 2
 for x in day1_input_data:
     for y in day1_input_data:
-        if x != y and x + y == 2020:
-            result.append(x*y)
+        if x != y:
+            for z in day1_input_data:
+                if z != y and x + y + z == 2020:
+                    result.append(x*y*z)
 
 if result[0] == result[1]:
     print(result[0])
