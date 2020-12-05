ef34ca7db82ea6af158cbf2fb6d5196576d2ecc1
Alexander Clements <alexfjclements@gmail.com>
2020-12-05 11:00:56 +0000

    day one part 1 complete

--- day_1/day1_main.py
+++ day_1/day1_main.py
@@ -1,2 +1,13 @@
 from day_1.day1_input import day1_input_data
 
+result = []
+
+for x in day1_input_data:
+    for y in day1_input_data:
+        if x != y and x + y == 2020:
+            result.append(x*y)
+
+if result[0] == result[1]:
+    print(result[0])
+else:
+    print('Error')
