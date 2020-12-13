b9be4164c3216204c5368bed0e9725aa38dd181a
Alexander Clements <alexfjclements@gmail.com>
2020-12-13 21:23:56 +0000

    day two part 2 completed

--- day_2/day_2_main.py
+++ day_2/day_2_main.py
@@ -2,12 +2,16 @@ from day_2.day_2_data import formatted_data
 
 
 # return number of validated passwords
-def password_checker(input_data):
+def password_checker(input_data, section_one=True):
 
     valid_number = 0
     for password in input_data:
-        if validity_check(password):
-            valid_number += 1
+        if section_one:
+            if validity_check(password):
+                valid_number += 1
+        else:
+            if updated_validity_check(password):
+                valid_number += 1
 
     return valid_number
 
@@ -34,4 +38,25 @@ def validity_check(password_details):
         return False
 
 
-print(password_checker(formatted_data))
+def updated_validity_check(password_details):
+    first_split = password_details.split('-', 1)
+    pos_one = int(first_split[0]) - 1
+
+    second_split = first_split[1].split(' ', 1)
+    pos_two = int(second_split[0]) - 1
+
+    third_split = second_split[1].split(':', 1)
+    letter = third_split[0]
+
+    password = third_split[1].split(' ')[1]
+
+    if password[pos_one] == letter or password[pos_two] == letter:
+        if not password[pos_one] == password[pos_two]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+print(password_checker(formatted_data, False))
